1d5b900a33ea3218cc3cc2af9e4a7a496cdeaa17
Carlos Emanuel Marroquin Ortega <carlosema322@gmail.com>
2021-03-31 15:51:56 -0600

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace aaa3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
